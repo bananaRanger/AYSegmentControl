d58300d4d50e933a20f20426db101a804c8c6649
Anton Yereshchenko <anton.ereshchenko@powercode.us>
2019-06-12 11:37:27 +0300

    initial commit

---
+++ Example/AYSegmentControl/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AYSegmentControl
+//
+//  Created by antonyereshchenko@gmail.com on 06/12/2019.
+//  Copyright (c) 2019 antonyereshchenko@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Pods/Target Support Files/AYSegmentControl/AYSegmentControl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AYSegmentControl : NSObject
+@end
+@implementation PodsDummy_AYSegmentControl
+@end
